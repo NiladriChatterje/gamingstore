5473659704aaa456d4b7bccb023516e2e4b97ba3
NiladriChatterje <radongtxgpu@gmail.com>
2024-09-28 12:47:06 +0530

    clerk publish key issue due to development instance

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
+// import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from 'react-router-dom'
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if (!PUBLISHABLE_KEY)
-  throw new Error("Missing Publishable Key")
+// if (!PUBLISHABLE_KEY)
+//   throw new Error("Missing Publishable Key")
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={"/"}>
-      <BrowserRouter><App /></BrowserRouter>
-    </ClerkProvider>
+    {/* <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={"/"}> */}
+    <BrowserRouter><App /></BrowserRouter>
+    {/* </ClerkProvider> */}
   </StrictMode>,
 )
