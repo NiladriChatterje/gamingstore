3fcef34a551ff3d9eba44c8386a52a6b7e9f608b
NiladriChatterje <radongtxgpu@gmail.com>
2025-01-26 13:15:00 +0530

    basic template of product addition is implemented

--- src/component/AdminAcount/AddProduct/AddProduct.tsx
+++ src/component/AdminAcount/AddProduct/AddProduct.tsx
@@ -1,8 +1,11 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import styles from './AddProduct.module.css';
-import { IoIosPersonAdd } from "react-icons/io";
+import { IoIosArrowDropdownCircle, IoIosPersonAdd } from "react-icons/io";
+import { AiFillProduct } from "react-icons/ai";
+import { MdConfirmationNumber, MdModelTraining } from "react-icons/md";
+import toast from "react-hot-toast";
 
-enum EanUpcIsbn { EAC = "EAC", UPC = "UPC", ISBN = "ISBN", ASIN = "ASIN", GTIN = "GTIN", OTHERS = "OTHERS" }
+enum EanUpcIsbn { EAN = "EAN", UPC = "UPC", ISBN = "ISBN", ASIN = "ASIN", GTIN = "GTIN", OTHERS = "OTHERS" }
 enum currency { INR = "INR", YEN = "YEN", USD = "USD" }
 
 type productPriceType = {
@@ -24,9 +27,15 @@ interface productType {
 }
 
 const AddProduct = () => {
+    const [modelNumber, setModelNumber] = useState<string>('');
+    const [eanUpc, setEacUpc] = useState<string>('')
+    const [eacUpcType, setEacUpcType] = useState<EanUpcIsbn>(EanUpcIsbn.EAN);
+    const [toggleEacUpcType, setToggleEacUpcType] = useState<boolean>(true);//close the dropdown
 
     async function handleSubmitPdt(e: FormEvent) {
         e.preventDefault();
+        toast('submitting')
+        return;
         await fetch('http://localhost:5000/add-product', {
             method: 'POST',
             headers: {
@@ -37,7 +46,69 @@ const AddProduct = () => {
     }
 
     return (
-        <form onSubmit={handleSubmitPdt}>
+        <form onSubmit={handleSubmitPdt}
+            id={styles['form-container']}>
+            <div id={styles['form-input-field-container']}>
+                <section data-label="product-details">
+                    <fieldset style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+                        <legend>Product Details</legend>
+                        <section>
+                            <div
+                                style={{ backgroundColor: 'rgba(255, 255, 255, 0.963)' }}
+                                data-section={'product-name'}
+                                className={styles['input-containers']}>
+                                <AiFillProduct />
+                                <input
+
+                                    name={'product-name'} placeholder={'Product Name'}
+                                    maxLength={6} minLength={6}
+                                    type='text' />
+                            </div>
+                        </section>
+                        <section
+                            style={{ display: 'flex', gap: 10, width: '100%' }}>
+                            <div style={{ maxWidth: 120, backgroundColor: 'rgba(255, 255, 255, 0.963)' }}
+                                data-section={'eanUpcIsbnGtinAsinType'}
+                                className={styles['input-containers']}>
+                                <IoIosArrowDropdownCircle cursor={'pointer'}
+                                    onClick={() => setToggleEacUpcType(prev => !prev)}
+                                />
+                                {toggleEacUpcType && <section className={`${toggleEacUpcType ? '' : styles['product-identification-list']}`}>
+                                    <dl onClick={() => { setEacUpcType(EanUpcIsbn.EAN); setToggleEacUpcType(true) }}>EAN</dl>
+                                    <dl onClick={() => { setEacUpcType(EanUpcIsbn.UPC); setToggleEacUpcType(true) }}>UPC</dl>
+                                    <dl onClick={() => { setEacUpcType(EanUpcIsbn.ISBN); setToggleEacUpcType(true) }}>ISBN</dl>
+                                    <dl onClick={() => { setEacUpcType(EanUpcIsbn.ASIN); setToggleEacUpcType(true) }}>ASIN</dl>
+                                    <dl onClick={() => { setEacUpcType(EanUpcIsbn.GTIN); setToggleEacUpcType(true) }}>GTIN</dl>
+                                </section>}
+                            </div>
+                            <div
+                                style={{ backgroundColor: 'rgba(255, 255, 255, 0.963)' }}
+                                data-section={'EAC-UPC-ISBN-ASIN-OTHERS-Identification-Number'}
+                                className={styles['input-containers']}>
+                                <MdConfirmationNumber />
+                                <input
+                                    value={eanUpc}
+                                    onChange={e => { setEacUpc(e.target.value) }}
+                                    name={'ean-upc'} placeholder={'EAN | UPC | ISBN | ASIN | GTIN | OTHERS'} type='text' />
+                            </div>
+                        </section>
+                        <section>
+                            <div
+                                data-section={'modelNumber'}
+                                style={{ backgroundColor: 'rgba(255, 255, 255, 0.963)' }}
+                                className={styles['input-containers']}>
+                                <MdModelTraining />
+                                <input value={modelNumber}
+                                    onChange={e => {
+                                        setModelNumber(e.target.value)
+                                    }}
+                                    name={'model-number'} placeholder={'model number'} type='text'
+                                />
+                            </div>
+                        </section>
+                    </fieldset>
+                </section>
+            </div>
             <section style={{ display: 'flex', justifyContent: 'flex-end', gap: 15 }}>
                 <IoIosPersonAdd
                     color='white'
@@ -47,10 +118,7 @@ const AddProduct = () => {
                         borderRadius: 5, padding: '5px 10px'
                     }}
                     size={25}
-                    // type={'submit'}
-                    onClick={() => {
-
-                    }}
+                    type={'submit'}
                 />
             </section>
         </form>
