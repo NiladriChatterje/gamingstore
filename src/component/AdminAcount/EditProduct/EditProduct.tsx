1d14167cfd7bc7d520c20485486d08655b464192
NiladriChatterje <radongtxgpu@gmail.com>
2025-02-03 20:43:11 +0530

    in progress

--- src/component/AdminAcount/EditProduct/EditProduct.tsx
+++ src/component/AdminAcount/EditProduct/EditProduct.tsx
@@ -1,15 +1,23 @@
 // import styles from './EditProduct.module.css';
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAdminStateContext } from "../AdminStateContext";
 
 const EditProduct = () => {
     const navigate = useNavigate();
     const [products, setProducts] = useState<any[]>(() => [1, 2, 3, 4, 5])
+    const { admin } = useAdminStateContext();
 
     useEffect(() => {
         async function getProductList() {
+            fetch(`http://localhost:5000/${admin._id}/fetch-products`, {
+                method: 'GET',
+                headers: {
 
+                }
+            })
         }
+        getProductList()
     }, [])
 
     return (<>
