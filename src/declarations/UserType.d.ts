9ed7e398a5f3b329a661dfca47b3d755059156f1
NiladriChatterje <radongtxgpu@gmail.com>
2025-06-09 23:25:40 +0530

    cart field added in userType declaration

--- src/declarations/UserType.d.ts
+++ src/declarations/UserType.d.ts
@@ -1,16 +1,19 @@
+import { ProductType } from "./ProductContextType";
+
 export declare type UserType = {
-   _id: string;
-    username: string | null | undefined;
-    geoPoint: {
-      lat: number;
-      lng: number;
-    }
-    phone?: number;
-    email: string | null | undefined;
-    address: {
-      pinCode: string;
-      county: string;
-      country: string;
-      state: string;
-    }
+  _id: string;
+  username: string | null | undefined;
+  geoPoint: {
+    lat: number;
+    lng: number;
+  }
+  phone?: number;
+  email: string | null | undefined;
+  address: {
+    pinCode: string;
+    county: string;
+    country: string;
+    state: string;
+  },
+  cart: ProductType[]
 }
